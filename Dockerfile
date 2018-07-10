6119fc203cb2c5c907e60d2c6bf6da614cdfe22f
Matthieu Giraud <matthieu.giraud@superdev.fr>
2018-07-10 10:12:46 +0200

    Server SPA : test performance

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,13 @@
 # stage 0
 FROM golang:latest as builder
-WORKDIR /go/src/github.com/PierreZ/goStatic
+WORKDIR /go
 COPY . .
 
-RUN GOARCH=amd64 GOOS=linux go build  -ldflags "-linkmode external -extldflags -static -w"
+RUN go get github.com/gorilla/mux
+RUN GOARCH=amd64 GOOS=linux go build -o gostatic  -ldflags "-linkmode external -extldflags -static -w"
 
 # stage 1
 FROM centurylink/ca-certs
 WORKDIR /
-COPY --from=builder /go/src/github.com/PierreZ/goStatic/goStatic .
-ENTRYPOINT ["/goStatic"]
+COPY --from=builder /go .
+ENTRYPOINT ["/gostatic","-dir","/app"]
--- main.go
+++ main.go
@@ -1,87 +1,54 @@
-// This small program is just a small web server created in static mode
-// in order to provide the smallest docker image possible
-
 package main
 
 import (
 	"flag"
 	"log"
 	"net/http"
-	"strconv"
-	"strings"
+	"time"
+    "fmt"
+	"github.com/gorilla/mux"
+    "os"
 )
 
 var (
-	// Def of flags
-	portPtr                  = flag.Int("port", 8043, "The listening port")
-	context                  = flag.String("context", "", "The 'context' path on which files are served, e.g. 'doc' will serve the files at 'http://localhost:<port>/doc/'")
-	path                     = flag.String("path", "/srv/http", "The path for the static files")
-	headerFlag               = flag.String("append-header", "", "HTTP response header, specified as `HeaderName:Value` that should be added to all responses.")
-	basicAuth                = flag.Bool("enable-basic-auth", false, "Enable basic auth. By default, password are randomly generated. Use --set-basic-auth to set it.")
-	setBasicAuth             = flag.String("set-basic-auth", "", "Define the basic auth. Form must be user:password")
-	defaultUsernameBasicAuth = flag.String("default-user-basic-auth", "gopher", "Define the user")
-	sizeRandom               = flag.Int("password-length", 16, "Size of the randomized password")
-
-	username string
-	password string
+        dir string
+        defaultFile string
 )
-
-func parseHeaderFlag(headerFlag string) (string, string) {
-	if len(headerFlag) == 0 {
-		return "", ""
-	}
-	pieces := strings.SplitN(headerFlag, ":", 2)
-	if len(pieces) == 1 {
-		return pieces[0], ""
-	}
-	return pieces[0], pieces[1]
-}
 
 func main() {
 
-	flag.Parse()
+    flag.StringVar(&dir, "dir", ".", "the directory to serve files from. Defaults to the current dir")
+    flag.Parse()
+    defaultFile = dir + "/index.html"
 
-	// sanity check
-	if len(*setBasicAuth) != 0 && !*basicAuth {
-		*basicAuth = true
+    r := mux.NewRouter()
+
+    r.PathPrefix("/").HandlerFunc(StaticHandler)
+    r.NotFoundHandler = http.HandlerFunc(NotFoundHandler)
+
+	fmt.Printf("Starting golang server\n")
+
+    srv := &http.Server{
+        Handler:      r,
+        Addr:         "0.0.0.0:8080",
+        WriteTimeout: 15 * time.Second,
+        ReadTimeout:  15 * time.Second,
+    }
+
+    log.Fatal(srv.ListenAndServe())
+}
+
+func NotFoundHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Printf("File not found %s",r.URL)
+    http.ServeFile(w, r, defaultFile)
+}
+
+func StaticHandler(w http.ResponseWriter, r *http.Request) {
+	path := dir + r.URL.EscapedPath()
+
+	if _, err := os.Stat(path); err == nil {
+		http.ServeFile(w, r, path)
+	} else {
+		http.ServeFile(w, r, defaultFile)
 	}
-
-	port := ":" + strconv.FormatInt(int64(*portPtr), 10)
-	
-	handler := http.FileServer(http.Dir(*path))
-	
-	pathPrefix := "/";
-	if len(*context) > 0 {
-		pathPrefix = "/"+*context+"/"
-		handler = http.StripPrefix(pathPrefix, handler)
-	}
-
-	if *basicAuth {
-		log.Println("Enabling Basic Auth")
-		if len(*setBasicAuth) != 0 {
-			parseAuth(*setBasicAuth)
-		} else {
-			generateRandomAuth()
-		}
-		handler = authMiddleware(handler)
-	}
-
-	// Extra headers.
-	if len(*headerFlag) > 0 {
-		header, headerValue := parseHeaderFlag(*headerFlag)
-		if len(header) > 0 && len(headerValue) > 0 {
-			fileServer := handler
-			handler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-				w.Header().Set(header, headerValue)
-				fileServer.ServeHTTP(w, r)
-			})
-		} else {
-			log.Println("appendHeader misconfigured; ignoring.")
-		}
-	}
-
-	http.Handle(pathPrefix, handler)
-
-	log.Printf("Listening at 0.0.0.0%v %v...", port, pathPrefix)
-	log.Fatalln(http.ListenAndServe(port, nil))
 }
